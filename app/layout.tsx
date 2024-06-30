fa90a437d8fa825a4e619f95c1c596f73fde5df4
Noel Fischer <noelfischer@gmx.ch>
2024-06-30 20:18:22 +0200

    Chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'dnd inventory',
+  description: 'A customizable inventory management system for D&D campaigns.',
+  metadataBase: new URL('https://dnd-inventory.vercel.app/'),
+};
 
 export default function RootLayout({
   children,

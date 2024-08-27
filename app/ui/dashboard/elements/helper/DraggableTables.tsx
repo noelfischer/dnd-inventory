9834ef162f03d00fee42fb4c0c4cdf9541314741
Noel Fischer <noelfischer@gmx.ch>
2024-08-27 19:42:01 +0200

    Make draggableTables usable

--- app/ui/dashboard/elements/helper/DraggableTables.tsx
+++ app/ui/dashboard/elements/helper/DraggableTables.tsx
@@ -1,35 +1,54 @@
 'use client'
 
 import React, { useState, DragEvent } from 'react';
-import { Table as UiTable, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { GripVertical, Ham, PencilRuler, Pickaxe, Shield, Sword } from 'lucide-react';
+import { InventoryItem } from '@/app/lib/definitions';
 
 
 // Define type for table and row structure
-type Table = {
+
+type TableProps = {
   id: number;
-  rows: string[];
+  name?: string;
+  rows: InventoryItem[];
 };
 
-const DraggableTables = () => {
-  const [tables, setTables] = useState<Table[]>([
-    { id: 1, rows: ['Row 1', 'Row 2', 'Row 3'] },
-    { id: 2, rows: ['Row A', 'Row B', 'Row C'] },
-  ]);
+const DraggableTables = ({ initialItems }: { initialItems: InventoryItem[] }) => {
+
+  function formatInitialItemstoTableData(initialItems: InventoryItem[]) {
+    const tableData: TableProps[] = [];
+    tableData.push({ id: 1, name: "Equiped", rows: initialItems.filter(i => i.slot == "eq") });
+    tableData.push({ id: 2, name: "On Body", rows: initialItems.filter(i => i.slot == "bd") });
+    tableData.push({ id: 3, name: "Backpack", rows: initialItems.filter(i => i.slot == "bp") });
+    return tableData;
+  }
+
+  const [tables, setTables] = useState<TableProps[]>(formatInitialItemstoTableData(initialItems));
 
 
   const [draggedRow, setDraggedRow] = useState<{ tableId: number; rowIndex: number } | null>(null);
   const [draggedOverTableId, setDraggedOverTableId] = useState<number | null>(null);
   const [draggedOverRowIndex, setDraggedOverRowIndex] = useState<number | null>(null);
 
-  const handleDragStart = (e: DragEvent<HTMLTableRowElement>, tableId: number, rowIndex: number) => {
+  const handleDragStart = (e: DragEvent<HTMLSpanElement>, tableId: number, rowIndex: number) => {
+    console.log('Drag start', tableId, rowIndex);
     setDraggedRow({ tableId, rowIndex });
+    // Set the drag image to the entire row
+    const rowElement = e.currentTarget.closest('tr') as HTMLTableRowElement;
+    console.log('rowElement', rowElement);
+    if (rowElement) {
+      e.dataTransfer.setDragImage(rowElement, rowElement.clientWidth - 30, 0);
+      e.dataTransfer.effectAllowed = 'move';
+    }
+    e.stopPropagation(); // Prevent row drag
   };
 
   const handleDragOver = (e: DragEvent<HTMLTableElement>, targetTableId: number) => {
     e.preventDefault();
     console.log('Drag over', e.target);
     setDraggedOverTableId(targetTableId);
-    setDraggedOverRowIndex(parseInt(((e.target as HTMLTableRowElement)).id));
+    setDraggedOverRowIndex(parseInt(((e.target as HTMLTableCellElement)).id));
   };
 
   const handleDrop = (e: DragEvent<HTMLTableElement>, targetTableId: number) => {
@@ -41,7 +60,7 @@ const DraggableTables = () => {
 
     const targetTable = tables.find((table) => table.id === targetTableId);
     if (!targetTable) return;
-    const targetIndex = parseInt(((e.target as HTMLTableRowElement)).id);
+    const targetIndex = parseInt(((e.target as HTMLTableCellElement)).id);
     console.log('targetIndex', targetIndex);
 
 
@@ -98,34 +117,81 @@ const DraggableTables = () => {
     <div>
       {tables.map((table) => (
         <div>
-          <h3 className='text-text text-lg bg-main border-y-2 px-4 border-black font-medium'>Table {table.id}</h3>
-          <UiTable
+          <h3 className='text-text text-lg bg-main border-y-2 px-4 border-black font-medium'>{table.name}</h3>
+          <Table
+            id='-1'
             key={table.id}
             onDragOver={(e) => handleDragOver(e, table.id)}
             onDrop={(e) => handleDrop(e, table.id)}
           >
-            <TableHeader>
-              <TableRow className={`${(draggedOverTableId === table.id && draggedOverRowIndex === -1) ? '!bg-main/20' : ''}`}>
-                <TableHead id='-1'>Name</TableHead>
+            <TableHeader id='-1'>
+              <TableRow id='-1' className={`${(draggedOverTableId === table.id && draggedOverRowIndex === -1) ? '!bg-main/20' : ''}`}>
+                <TableHead id='-1' className="w-[100px]">Name</TableHead>
+                <TableHead id='-1' >Description</TableHead>
+                <TableHead id='-1' >Ability</TableHead>
+                <TableHead id='-1' >Magic</TableHead>
+                <TableHead id='-1' >Weight</TableHead>
+                <TableHead id='-1' >Quantity</TableHead>
+                <TableHead id='-1' className="text-right">Actions</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
               {table.rows.map((row, index) => (
                 <TableRow
                   key={index}
-                  draggable
-                  onDragStart={(e) => handleDragStart(e, table.id, index)}
-                  className={`p-2 cursor-move ${(draggedOverTableId === table.id && (draggedOverRowIndex === index || draggedOverRowIndex === -1)) ? '!bg-main/20' : ''} ${draggedRow && draggedRow.tableId === table.id && draggedRow.rowIndex === index ? '!bg-mainAccent/30' : ''}`}
+                  className={`p-2 transition-all ${(draggedOverTableId === table.id && (draggedOverRowIndex === index || draggedOverRowIndex === -1)) ? '!bg-main/20' : ''} ${draggedRow && draggedRow.tableId === table.id && draggedRow.rowIndex === index ? '!bg-mainAccent/30' : ''}`}
                 >
-                  <TableCell id={index.toString()}>{row}</TableCell>
+                  <TableCell className="font-base flex gap-2">{selectIcon(row.category)} {row.item_name}</TableCell>
+                  <TableCell>{row.description}</TableCell>
+                  <TableCell>{row.ability}</TableCell>
+                  <TableCell>{row.magic}</TableCell>
+                  <TableCell>{row.weight} kg</TableCell>
+                  <TableCell>{row.quantity}</TableCell>
+                  <TableCell id={index.toString()} className=' flex flex-row-reverse'>
+                    <div id={index.toString()} onDragStart={(e) => handleDragStart(e, table.id, index)}
+                      className='cursor-move h-6 w-6' draggable>
+                      <div className='absolute'>
+                        <div className='absolute top-0 h-7 w-6 z-10' id={index.toString()} />
+                        <GripVertical className='absolute top-0 z-0' />
+                      </div>
+                    </div>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
-          </UiTable>
+            {table.name === 'Backpack' &&
+              <TableFooter id='-1'>
+                <TableRow id='-1'>
+                  <TableCell id='-1' colSpan={6}>Total Weight</TableCell>
+                  <TableCell id='-1' className="text-right">87%</TableCell>
+                </TableRow>
+              </TableFooter>
+            }
+          </Table>
         </div>
       ))}
     </div>
   );
 };
+
+export function selectIcon(category: string) {
+  switch (category) {
+    // Weapons
+    case 'W':
+      return <Sword />;
+    // Armor
+    case 'A':
+      return <Shield />;
+    // Consumables
+    case 'C':
+      return <Ham />;
+    // Tools
+    case 'T':
+      return <Pickaxe />;
+    // Default
+    default:
+      return <PencilRuler />;
+  }
+}
 
 export default DraggableTables;
